e3d6339acc7102d80f9dd75717092afb0309fa44
Carolina Lopez <carolina.lope@hotmail.es>
2022-06-06 16:41:05 +0200

    ruta crear booking problemas con el checkin

--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -1,32 +1,11 @@
 const BookingModel = require("../models/Booking.Model");
 const HotelModel = require("../models/Hotel.Model");
-const UserModel = require("../models/User.model");
 const isAuthenticated = require("../middleware/isAuthenticated")
+const checkin = require("../utils/checkin.js")
 
 const router = require("express").Router();
 
 //CRUD DEL BOOKING
-
-//GET "/api/hotels/:id/booking"=> visualizar bookings
-router.get("/:id/booking", isAuthenticated, async (req, res, next) =>{
-
-    const {id} = req.params
-    const {_id} = req.payload
-    
-
-    try {
-        const dataHotel = await HotelModel.findById(id).select("precios pension")
-        const bookingtHotel = await BookingModel.find({hotelId: id}).populate("clienteId", "username")
-        res.json({
-            dataHotel,
-            bookingtHotel
-        })
-        
-    } catch (error) {
-        next(error)
-    }
-})
-
 
 
 //POST "/api/hotels/:id/booking" => crear el booking
@@ -54,6 +33,30 @@ console.log(req.body)
         next(error)
     }
 })
+
+//GET "/api/hotels/:id/booking"=> visualizar bookings
+router.get("/:id/booking", isAuthenticated, async (req, res, next) =>{
+
+    const {id} = req.params
+    const {_id} = req.payload
+    
+
+    try {
+        const dataHotel = await HotelModel.findById(id).select("precios pension")
+        const bookingtHotel = await BookingModel.find({hotelId: id}).populate("clienteId", "username")
+        res.json({
+            dataHotel,
+            bookingtHotel
+        })
+        
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+
 
 
 
